8402425e7cf8f015ff79891b3be62bc4b68e4b42
concario17-max <concario17@gmail.com>
2026-03-18 11:43:55 +0900

    Remove password gate

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,12 @@ import React from 'react';
 import TextPage from './pages/TextPage';
 import { UIProvider } from './context/UIContext';
 import Header from './components/Header';
-import PasswordGuard from './components/PasswordGuard';
 
 function App() {
   return (
     <UIProvider>
-      <PasswordGuard>
-        <Header />
-        <TextPage />
-      </PasswordGuard>
+      <Header />
+      <TextPage />
     </UIProvider>
   );
 }
